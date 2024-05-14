8ffe385d737f6a6eee248ee05d60964f49580fce
Yasela-dissanayake <yasela2014@gmail.com>
2024-05-14 19:12:47 +0530

    feat: quick options icons added

--- frontend/src/components/RightBar.tsx
+++ frontend/src/components/RightBar.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import IconButton from "@mui/material/IconButton";
+import Badge from "@mui/material/Badge";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsIcon from "@mui/icons-material/Settings";
+// import RightBarProfile from "./RightBarProfile";
 
-const RightBar = () => {
-  return <div>RightBar</div>;
+const RightBar: React.FC = () => {
+  return (
+    <div className="px-6  pt-4 hidden lg:flex flex-col bg-white rounded-l-2xl w-[350px] ml-auto ">
+      <div className="grid grid-cols-3 gap-4 items-center mx-5">
+        <div>
+          <IconButton aria-label="show new notifications" color="inherit">
+            <Badge badgeContent={5} color="error">
+              <NotificationsNoneIcon />
+            </Badge>
+          </IconButton>
+        </div>
+        <div>
+          <IconButton aria-label="show new notifications" color="inherit">
+            <SettingsIcon />
+          </IconButton>
+        </div>
+        <div className="">{/* <RightBarProfile /> */}</div>
+      </div>
+    </div>
+  );
 };
 
 export default RightBar;
