32319979598931f9463bfb95f3220908eb6308fe
Methmina Binara <binaraepa@gmail.com>
2024-05-14 18:38:31 +0530

    feat: Added logo to landing page

--- frontend/src/views/LandingPage.tsx
+++ frontend/src/views/LandingPage.tsx
@@ -2,7 +2,17 @@ import React from "react";
 import logo from "../assets/images/logo.png";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div>
+      <div className="flex flex-col justify-start items-center h-screen bg-[#F5F5F5]">
+        <div className="mt-10">
+          <img src={logo} alt="Materny Logo" className="w-24" />
+        </div>
+
+        
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
