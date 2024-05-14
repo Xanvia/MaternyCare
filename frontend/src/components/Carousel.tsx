8d32ebe609ccf082fd3ccc0b51f3a967d7518c64
Methmina Binara <binaraepa@gmail.com>
2024-05-14 19:00:52 +0530

    feat: Started creating the carousel

---
+++ frontend/src/components/Carousel.tsx
@@ -0,0 +1,8 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
