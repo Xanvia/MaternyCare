834ca0f4069d7e00d4d4701aa2660ce146096f0d
Methmina Binara <binaraepa@gmail.com>
2024-05-14 18:43:56 +0530

    feat: Added name

--- frontend/src/views/LandingPage.tsx
+++ frontend/src/views/LandingPage.tsx
@@ -6,7 +6,13 @@ const LandingPage = () => {
           <img src={logo} alt="Materny Logo" className="w-24" />
         </div>
 
-        
+        <div className="mt-0 text-center leading-9 font-semibold">
+          <h1 style={{ fontFamily: "Ubuntu", fontSize: "2.25rem" }}>
+            <span style={{ color: "#0D99FF" }}>Materny</span>
+            <span style={{ color: "#F580AB" }}>Care</span>
+          </h1>
+        </div>
+
       </div>
     </div>
   );
