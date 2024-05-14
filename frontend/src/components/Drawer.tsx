252c0fe4026cb021c662b7295accd201ea3d7e29
Yasela-dissanayake <yasela2014@gmail.com>
2024-05-14 18:40:59 +0530

    feat: menu icon added to drawer

--- frontend/src/components/Drawer.tsx
+++ frontend/src/components/Drawer.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material//styles";
+import { MenuIcon } from "../assets/icons/Icons";
 
 const Drawer: React.FC = () => {
   const LARGE_SCREEN_WIDTH = 1024;
@@ -30,7 +31,20 @@ const Drawer: React.FC = () => {
     };
   }, []);
 
-  return <div>Drawer</div>;
+  return (
+    <div>
+      {" "}
+      <div className="text-center absolute right-3 top-5 lg:hidden ">
+        <button
+          className="text-[#0D99FF] bg-[#B3DFFF] focus:ring-2 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-3 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          type="button"
+          onClick={toggleDrawer}
+        >
+          <MenuIcon className="" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Drawer;
