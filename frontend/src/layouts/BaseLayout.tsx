60b8ab35e0f8d5ffc9bcc86c6e5b48a9eda5853d
Yasela-dissanayake <yasela2014@gmail.com>
2024-05-14 17:17:27 +0530

    feat:Drawer and Rightbar linked to base layout

---
+++ frontend/src/layouts/BaseLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Drawer from "../components/Drawer";
+import RightBar from "../components/RightBar";
+
+const BaseLayout = () => {
+  return (
+    <div className="flex bg-[#F5F5F5]">
+      <Drawer />
+      <div className="lg:ml-[300px] ml-0 w-full ">
+        <Outlet />
+      </div>
+      <RightBar />
+    </div>
+  );
+};
+
+export default BaseLayout;
