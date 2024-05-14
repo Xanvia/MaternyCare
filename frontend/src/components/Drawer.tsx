deca591eef4a99eed4663a9c129a4a5d3d1e803b
Yasela-dissanayake <yasela2014@gmail.com>
2024-05-14 18:39:47 +0530

    feat: added drawer custom toggle to lg breakpoint with useeffect

--- frontend/src/components/Drawer.tsx
+++ frontend/src/components/Drawer.tsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material//styles";
 
-const Drawer = () => {
+const Drawer: React.FC = () => {
+  const LARGE_SCREEN_WIDTH = 1024;
+
+  const theme = useTheme();
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up("lg"));
+  const [isDrawerOpen, setDrawerOpen] = React.useState(isLargeScreen);
+
+  const checkScreenSize = () => {
+    if (window.innerWidth >= LARGE_SCREEN_WIDTH) {
+      setDrawerOpen(true);
+    } else {
+      setDrawerOpen(false);
+    }
+  };
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!isDrawerOpen);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checkScreenSize);
+    checkScreenSize(); // Initial check
+
+    return () => {
+      window.removeEventListener("resize", checkScreenSize);
+    };
+  }, []);
+
   return <div>Drawer</div>;
 };
 
