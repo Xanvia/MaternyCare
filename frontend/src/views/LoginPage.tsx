d14e2b90ba6f9675f90f1b9564f34e19cbe86eaa
Sahan-Maleesha <smaleesha84@gmail.com>
2024-05-15 12:05:08 +0530

    feat:Added formik package

--- frontend/src/views/LoginPage.tsx
+++ frontend/src/views/LoginPage.tsx
@@ -1,5 +1,49 @@
-const LoginPage = () => {
-  return <div>LoginPage</div>;
+import React from "react";
+import { useFormik } from "formik";
+
+interface FormValues {
+  name: string;
+  password: string;
+}
+
+const LoginPage: React.FC = () => {
+  const formik = useFormik<FormValues>({
+    initialValues: {
+      name: "",
+      password: "",
+    },
+    onSubmit: values => {
+      console.log('Form data', values);
+    }
+  });
+
+  return (
+    <form autoComplete="off" onSubmit={formik.handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          value={formik.values.name}
+          placeholder="Name"
+          id="name"
+          name="name"
+          type="text"
+          onChange={formik.handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          value={formik.values.password}
+          placeholder="Password"
+          id="password"
+          name="password"
+          type="password"
+          onChange={formik.handleChange}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
 export default LoginPage;
