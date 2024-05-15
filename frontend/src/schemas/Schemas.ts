ba0f5dec1bf3143f50d72e34ec946e0b6bf71ac3
Yasela-dissanayake <yasela2014@gmail.com>
2024-05-16 00:36:28 +0530

    feat: error icon added to name field

--- frontend/src/schemas/Schemas.ts
+++ frontend/src/schemas/Schemas.ts
@@ -1,9 +1,12 @@
 import * as yup from "yup";
-import { ErrorIcon } from "../assets/icons/Icons";
 
 const passwordRules = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{5,}$/;
 
 export const loginSchema = yup.object().shape({
-    name: yup.string().required(`${ErrorIcon} Name is required!`),
-    password: yup.string().min(5).matches(passwordRules, {message: "Please Enter Strong Password!"}).required(`${ErrorIcon} Password is required!`),
+  name: yup.string().required("Name is required!"),
+  password: yup
+    .string()
+    .min(5)
+    .matches(passwordRules, { message: "Please Enter Strong Password!" })
+    .required("Password is required!"),
 });
--- frontend/src/views/LoginPage.tsx
+++ frontend/src/views/LoginPage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useFormik } from "formik";
 import logo from "../assets/images/logo.png";
 import { loginSchema } from "../schemas/Schemas";
-
+import { ErrorIcon } from "../assets/icons/Icons";
 
 interface FormValues {
   name: string;
@@ -16,60 +16,83 @@ const LoginPage: React.FC = () => {
       password: "",
     },
     validationSchema: loginSchema,
-    onSubmit: values => {
-      console.log('Form data', values);
-    }
+    onSubmit: (values) => {
+      console.log("Form data", values);
+    },
   });
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <form autoComplete="off" onSubmit={formik.handleSubmit} className="flex flex-col items-center w-full">
-      <img src={logo} alt="" />
-      <header className="text-blue_primary text-4xl mb-14">Materny<span className="text-pink_primary">Care</span></header>
-      <div className="w-full flex flex-col items-center mb-9">
-        <input
-          className={`shadow appearance-none rounded-b-xl py-4 px-4 w-11/12 lg:w-5/12 sm:w-8/12 ss:w-10/12 text-gray-700 leading-tight focus:shadow-outline text-lg
-          ${formik.touched.name && formik.errors.name ? 'border-solid border-red-500' : 'border-none'}`}
-          value={formik.values.name}
-          placeholder="Name"
-          id="name"
-          name="name"
-          type="text"
-          onChange={formik.handleChange}
-        />
-        <div className="w-11/12 lg:w-5/12 sm:w-8/12 ss:w-10/12 mb-9 flex flex-col items-start pl-4 mt-1">
-        {formik.touched.name && formik.errors.name ? (
-            <div className="text-red-500 text-xs">{formik.errors.name}</div>
-          ) : null}
+      <form
+        autoComplete="off"
+        onSubmit={formik.handleSubmit}
+        className="flex flex-col items-center w-full"
+      >
+        <img src={logo} alt="" />
+        <header className="text-blue_primary text-4xl mb-14">
+          Materny<span className="text-pink_primary">Care</span>
+        </header>
+        <div className="w-full flex flex-col items-center mb-9">
+          <input
+            className={`shadow appearance-none rounded-b-xl py-4 px-4 w-11/12 lg:w-5/12 sm:w-8/12 ss:w-10/12 text-gray-700 leading-tight focus:shadow-outline text-lg
+          ${
+            formik.touched.name && formik.errors.name
+              ? "border-solid border-red-500"
+              : "border-none"
+          }`}
+            value={formik.values.name}
+            placeholder="Name"
+            id="name"
+            name="name"
+            type="text"
+            onChange={formik.handleChange}
+          />
+          {/* Display error message with icon */}
+          <div className="w-11/12 lg:w-5/12 sm:w-8/12 ss:w-10/12 mb-9 flex flex-col items-start pl-4 mt-1">
+            {formik.touched.name && formik.errors.name ? (
+              <div className="text-red-500 text-xs">
+                {" "}
+                <ErrorIcon /> {formik.errors.name}
+              </div>
+            ) : null}
+          </div>
         </div>
-      </div>
-      <div className="w-full mb-9 flex flex-col items-center">
-        <input
-          className={`shadow appearance-none rounded-b-xl py-4 px-4 w-11/12 lg:w-5/12 sm:w-8/12 ss:w-10/12	 text-gray-700 leading-tight  focus:shadow-outline text-lg
-          ${formik.touched.name && formik.errors.name ? 'border-solid border-red-500' : 'border-none'}`}
-          value={formik.values.password}
-          placeholder="Password"
-          id="password"
-          name="password"
-          type="password"
-          onChange={formik.handleChange}
-        />
-        <div className="w-11/12 lg:w-5/12 sm:w-8/12 ss:w-10/12 mb-9 flex flex-col items-start pl-4 mt-1">
-        {formik.touched.password && formik.errors.password ? (
-            <div className="text-red-500 text-xs">{formik.errors.password}</div>
-          ) : null}
+        <div className="w-full mb-9 flex flex-col items-center">
+          <input
+            className={`shadow appearance-none rounded-b-xl py-4 px-4 w-11/12 lg:w-5/12 sm:w-8/12 ss:w-10/12	 text-gray-700 leading-tight  focus:shadow-outline text-lg
+          ${
+            formik.touched.name && formik.errors.name
+              ? "border-solid border-red-500"
+              : "border-none"
+          }`}
+            value={formik.values.password}
+            placeholder="Password"
+            id="password"
+            name="password"
+            type="password"
+            onChange={formik.handleChange}
+          />
+          <div className="w-11/12 lg:w-5/12 sm:w-8/12 ss:w-10/12 mb-9 flex flex-col items-start pl-4 mt-1">
+            {formik.touched.password && formik.errors.password ? (
+              <div className="text-red-500 text-xs">
+                {formik.errors.password}
+              </div>
+            ) : null}
+          </div>
         </div>
-      </div>
-      <div className="w-11/12 lg:w-5/12 sm:w-8/12 ss:w-10/12 mb-9 flex flex-col items-start">
-      <a href="" className="text-[#838383] text-xs ">Forgot Password?</a>
-      </div>
-      <button
-      className={`text-xl py-5 rounded-xl w-11/12 lg:w-5/12 sm:w-8/12 ss:w-10/12 text-white h-16 bg-blue_primary hover:bg-[#33C2FF]`} 
-      type="submit">Login
-      </button>
-    </form>
+        <div className="w-11/12 lg:w-5/12 sm:w-8/12 ss:w-10/12 mb-9 flex flex-col items-start">
+          <a href="" className="text-[#838383] text-xs ">
+            Forgot Password?
+          </a>
+        </div>
+        <button
+          className={`text-xl py-5 rounded-xl w-11/12 lg:w-5/12 sm:w-8/12 ss:w-10/12 text-white h-16 bg-blue_primary hover:bg-[#33C2FF]`}
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
     </div>
-    
   );
 };
 
