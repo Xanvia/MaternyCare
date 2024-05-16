0ccfb7bde32f4cb4ce21544aa665c626c16651a4
Yasela-dissanayake <yasela2014@gmail.com>
2024-05-16 13:54:48 +0530

    feat: updateNotices controller created

--- backend/src/controller/notice.controller.ts
+++ backend/src/controller/notice.controller.ts
@@ -123,3 +123,53 @@ export const createNotice = async (
       );
   }
 };
+
+export const updateNotice = async (
+  req: Request,
+  res: Response
+): Promise<Response<Notice>> => {
+  console.info(
+    `[${new Date().toLocaleString()}] Incoming ${req.method}${
+      req.originalUrl
+    } Request from ${req.rawHeaders[0]} ${req.rawHeaders[1]}`
+  );
+
+  let notice: Notice = { ...req.body };
+
+  try {
+    const pool = await connection();
+    const result: any = await pool.query(QUERY.SELECT_NOTICE, [
+      req.params.noticeId,
+    ]);
+
+    if ((result[0] as Array<any>).length > 0) {
+      const result: any = await pool.query(QUERY.UPDATE_NOTICE, [
+        ...Object.values(notice),
+        req.params.noticeId,
+      ]);
+      return res.status(Code.OK).send(
+        new HttpResponse(Code.OK, Status.OK, "Notice updated", {
+          ...notice,
+          id: req.params.noticeId,
+        })
+      );
+    } else {
+      return res
+        .status(Code.NOT_FOUND)
+        .send(
+          new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, "Notice not found")
+        );
+    }
+  } catch (error: unknown) {
+    console.error(error);
+    return res
+      .status(Code.INTERNAL_SERVER_ERROR)
+      .send(
+        new HttpResponse(
+          Code.INTERNAL_SERVER_ERROR,
+          Status.INTERNAL_SERVER_ERROR,
+          "An Error Occurred"
+        )
+      );
+  }
+};
