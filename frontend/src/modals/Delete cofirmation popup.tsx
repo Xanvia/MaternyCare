743f87ce6f72da962f3b64925fada190a8dd6045
Yasela-dissanayake <yasela2014@gmail.com>
2024-05-17 11:41:39 +0530

    fix: fixed mobile responsive issue

--- frontend/src/modals/Delete cofirmation popup.tsx
+++ frontend/src/modals/Delete cofirmation popup.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import Button from '@mui/joy/Button';
-import Divider from '@mui/joy/Divider';
-import DialogTitle from '@mui/joy/DialogTitle';
-import DialogContent from '@mui/joy/DialogContent';
-import DialogActions from '@mui/joy/DialogActions';
-import Modal from '@mui/joy/Modal';
-import ModalDialog from '@mui/joy/ModalDialog';
-import DeleteForever from '@mui/icons-material/DeleteForever';
-import WarningRoundedIcon from '@mui/icons-material/WarningRounded';
+import * as React from "react";
+import Button from "@mui/joy/Button";
+import Divider from "@mui/joy/Divider";
+import DialogTitle from "@mui/joy/DialogTitle";
+import DialogContent from "@mui/joy/DialogContent";
+import DialogActions from "@mui/joy/DialogActions";
+import Modal from "@mui/joy/Modal";
+import ModalDialog from "@mui/joy/ModalDialog";
+import DeleteForever from "@mui/icons-material/DeleteForever";
+import WarningRoundedIcon from "@mui/icons-material/WarningRounded";
 
-import IconButton from '@mui/joy/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
+import IconButton from "@mui/joy/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function AlertDialogModal() {
   const [open, setOpen] = React.useState<boolean>(false);
@@ -22,63 +22,95 @@ export default function AlertDialogModal() {
         endDecorator={<DeleteForever />}
         onClick={() => setOpen(true)}
         sx={{
-            borderColor: '#F580AB',
-            color: '#F580AB',
-            '&:hover': {
-              borderColor: '#F9B8D0',
-              color: '#F9B8D0',
-            },
-          }}
+          borderColor: "#F580AB",
+          color: "#F580AB",
+          "&:hover": {
+            borderColor: "#F9B8D0",
+            color: "#F9B8D0",
+          },
+        }}
       >
         Delete
       </Button>
       <Modal open={open} onClose={() => setOpen(false)}>
         <ModalDialog variant="outlined" role="alertdialog">
-        <IconButton
+          <IconButton
             aria-label="close"
             onClick={() => setOpen(false)}
             sx={{
-              position: 'absolute',
-              top: '8px',
-              right: '8px',
-              color: '#666666',
+              position: "absolute",
+              top: "8px",
+              right: "8px",
+              color: "#666666",
             }}
           >
             <CloseIcon />
           </IconButton>
-          <DialogTitle sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: 1, color:'#333333' }}>
+          <DialogTitle
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: 1,
+              color: "#333333",
+            }}
+          >
             <WarningRoundedIcon />
             Are you sure?
           </DialogTitle>
           <Divider />
-          <DialogContent sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: 1, color: '#666666' }}>
-            Are you sure you want to delete this item? This action cannot be undone
+          <DialogContent
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: 1,
+              color: "#666666",
+            }}
+          >
+            Are you sure you want to delete this item? This action cannot be
+            undone
           </DialogContent>
-          <DialogActions sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: 1, color: '#666666' }}>            
-          <Button variant="outlined"
-  sx={{
-    borderColor: '#F580AB',
-    color: '#000000',
-    '&:hover': {
-      borderColor: '#F9B8D0',
-    },
-    padding: { xs: '8px 16px', md: '12px 70px' }, // Adjust padding for different screen sizes
-    fontSize: '1rem',
-  }} onClick={() => setOpen(false)}>
-  Cancel
-</Button>
-<Button variant="solid"
-  sx={{
-    backgroundColor: '#F580AB',
-    color: '#ffffff',
-    '&:hover': {
-      backgroundColor: '#F9B8D0',
-    },
-    padding: { xs: '8px 16px', md: '12px 70px' }, // Adjust padding for different screen sizes
-    fontSize: '1rem',
-  }} onClick={() => setOpen(false)}>
-  Delete
-</Button>
+          <DialogActions
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: 1,
+              color: "#666666",
+            }}
+          >
+            <Button
+              variant="outlined"
+              sx={{
+                borderColor: "#F580AB",
+                color: "#000000",
+                "&:hover": {
+                  borderColor: "#F9B8D0",
+                },
+                width: { xs: "50%", md: "40%" },
+                fontSize: "1rem",
+              }}
+              onClick={() => setOpen(false)}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="solid"
+              sx={{
+                backgroundColor: "#F580AB",
+                color: "#ffffff",
+                "&:hover": {
+                  backgroundColor: "#F9B8D0",
+                },
+
+                width: { xs: "50%", md: "40%" },
+                fontSize: "1rem",
+              }}
+              onClick={() => setOpen(false)}
+            >
+              Delete
+            </Button>
           </DialogActions>
         </ModalDialog>
       </Modal>
