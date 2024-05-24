1ca5e79e74eacb3dd87361d3ea7f042ff36def8a
Sahan-Maleesha <smaleesha84@gmail.com>
2024-05-24 23:31:47 +0530

    WIP

--- frontend/src/schemas/Schemas.ts
+++ frontend/src/schemas/Schemas.ts
@@ -1,12 +1,12 @@
 import * as yup from "yup";
 
-const passwordRules = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{5,}$/;
+// const passwordRules = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{5,}$/;
 
 export const loginSchema = yup.object().shape({
-  name: yup.string().required("Name is required!"),
+  name: yup.string().required("Please provide your name."),
   password: yup
     .string()
     .min(5)
-    .matches(passwordRules, { message: "Please Enter Strong Password!" })
-    .required("Password is required!"),
+    // .matches(passwordRules, { message: "Please Enter Strong Password!" })
+    .required("Please enter your password."),
 });
