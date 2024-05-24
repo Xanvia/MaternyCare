c65f18650714534d29cb670de16a2d933fa8370b
Methmina Binara <binaraepa@gmail.com>
2024-05-24 18:01:25 +0530

    feat: Started creating add notice popup modal

---
+++ frontend/src/modals/Add notice popup.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Button from "@mui/joy/Button";
+import Divider from "@mui/joy/Divider";
+import DialogTitle from "@mui/joy/DialogTitle";
+import DialogContent from "@mui/joy/DialogContent";
+import DialogActions from "@mui/joy/DialogActions";
+import Modal from "@mui/joy/Modal";
+import ModalDialog from "@mui/joy/ModalDialog";
+import DeleteForever from "@mui/icons-material/DeleteForever";
+import WarningRoundedIcon from "@mui/icons-material/WarningRounded";
+
+import IconButton from "@mui/joy/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
