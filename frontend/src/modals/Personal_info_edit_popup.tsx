8c4eb45a76e82ebbf54e793a37e1a8d70d941376
Methmina Binara <binaraepa@gmail.com>
2024-05-25 23:36:36 +0530

    feat: Added email address section

--- frontend/src/modals/Personal_info_edit_popup.tsx
+++ frontend/src/modals/Personal_info_edit_popup.tsx
@@ -186,6 +186,33 @@ const validationSchema = Yup.object({
                     />
                   </Box>
 
+                  <DialogContent
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      gap: 1,
+                      color: "#666666",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Email address:
+                  </DialogContent>
+                  <Box
+                    sx={{
+                      width: 500,
+                      maxWidth: "100%",
+                    }}
+                  >
+                    <Field
+                      as={TextField}
+                      name="title"
+                      fullWidth
+                      size="small"
+                      variant="outlined"
+                      error={touched.title && Boolean(errors.title)}
+                      helperText={touched.title && errors.title}
+                    />
+                  </Box>
                   
                   <DialogActions
                     sx={{
