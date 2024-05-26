0c8054f18839449f3b3f6cee4911b4c9f6c75ec1
Yasela-dissanayake <yasela2014@gmail.com>
2024-05-26 12:11:59 +0530

    feat: guide drawer link added

--- frontend/src/data/Data.ts
+++ frontend/src/data/Data.ts
@@ -1,35 +1,41 @@
 import {
-    AppointmentsIcon,
-    DashboardIcon,
-    NoticesIcon,
-    ProfileIcon,
-    LogoutIcon
-  } from "../assets/icons/Icons";
-  
-  export const navLinks = [
-    {
-      name: "Dashboard",
-      icon: DashboardIcon,
-      path: "/dashboard",
-    },
-    {
-      name: "Notices",
-      icon: NoticesIcon,
-      path: "/notices",
-    },
-    {
-      name: "Appointments",
-      icon: AppointmentsIcon,
-      path: "/appointments",
-    },
-    {
-      name: "Profile",
-      icon: ProfileIcon,
-      path: "/profile",
-    },
-    {
-      name: "Logout",
-      icon: LogoutIcon,
-      path: "",
-    },
-  ];
+  AppointmentsIcon,
+  DashboardIcon,
+  NoticesIcon,
+  ProfileIcon,
+  LogoutIcon,
+  Help,
+} from "../assets/icons/Icons";
+
+export const navLinks = [
+  {
+    name: "Dashboard",
+    icon: DashboardIcon,
+    path: "/dashboard",
+  },
+  {
+    name: "Notices",
+    icon: NoticesIcon,
+    path: "/notices",
+  },
+  {
+    name: "Appointments",
+    icon: AppointmentsIcon,
+    path: "/appointments",
+  },
+  {
+    name: "Profile",
+    icon: ProfileIcon,
+    path: "/profile",
+  },
+  {
+    name: "Mother's Guide",
+    icon: Help,
+    path: "/guide",
+  },
+  {
+    name: "Logout",
+    icon: LogoutIcon,
+    path: "",
+  },
+];
