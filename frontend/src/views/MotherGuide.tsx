31013a92cde7102c1aa992b282e4a062dc114871
Yasela-dissanayake <yasela2014@gmail.com>
2024-05-26 12:07:08 +0530

    feat: masonory grid added

--- frontend/src/views/MotherGuide.tsx
+++ frontend/src/views/MotherGuide.tsx
@@ -1,7 +1,67 @@
 import React from "react";
 
-const MotherGuide = () => {
-  return <div className="mx-10">MotherGuide</div>;
+const categories = [
+  {
+    name: "All categories",
+    styles:
+      "text-blue-700 hover:text-white border border-blue-600 bg-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:bg-gray-900 dark:focus:ring-blue-800",
+  },
+  {
+    name: "Shoes",
+    styles:
+      "text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800",
+  },
+  {
+    name: "Bags",
+    styles:
+      "text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800",
+  },
+  {
+    name: "Electronics",
+    styles:
+      "text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800",
+  },
+  {
+    name: "Gaming",
+    styles:
+      "text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800",
+  },
+];
+
+const images = [
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg",
+];
+
+const MotherGuide: React.FC = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center py-4 md:py-8 flex-wrap">
+        {categories.map((category, index) => (
+          <button key={index} type="button" className={category.styles}>
+            {category.name}
+          </button>
+        ))}
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+        {images.map((src, index) => (
+          <div key={index}>
+            <img className="h-auto max-w-full rounded-lg" src={src} alt="" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MotherGuide;
