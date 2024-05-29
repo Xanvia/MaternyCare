8e5e35c221b4076e9531cee1aa284a3b0fe700a7
Methmina Binara <binaraepa@gmail.com>
2024-05-29 12:06:14 +0530

    Imported necessory MUI components

---
+++ frontend/src/modals/AccountInfoEditPopup.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Button from "@mui/joy/Button";
+import Divider from "@mui/joy/Divider";
+import DialogTitle from "@mui/joy/DialogTitle";
+import DialogContent from "@mui/joy/DialogContent";
+import DialogActions from "@mui/joy/DialogActions";
+import Modal from "@mui/joy/Modal";
+import ModalDialog from "@mui/joy/ModalDialog";
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/joy/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import { Box } from "@mui/material";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import { Edit } from "../assets/icons/Icons";
