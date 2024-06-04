c01c29319752f1080dd2a1e5b296bfd7b99dd838
Methmina Binara <binaraepa@gmail.com>
2024-06-05 00:20:08 +0530

    feat: Created content box

---
+++ frontend/src/views/Notification.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Notification = () => {
+  return (
+    <div className="flex justify-center items-start min-h-screen bg-background relative">
+      <div className="w-full max-w-2xl p-4 bg-purple_tertiary rounded-2xl shadow-md absolute top-0">
+        {"Hello"}
+      </div>
+    </div>
+  );
+}
+
+export default Notification;
