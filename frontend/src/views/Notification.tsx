9f5cb028af3bb9fed277a2a57f64e3799084edd2
Methmina Binara <binaraepa@gmail.com>
2024-06-07 22:59:43 +0530

    chore: Imported images

--- frontend/src/views/Notification.tsx
+++ frontend/src/views/Notification.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import MOH from '../assets/images/moh.png';
+import PHM from '../assets/images/phm.png';
 
 const Notification = () => {
   return (
-    <div className="flex justify-center items-start min-h-screen bg-background relative">
-      <div className="w-full max-w-2xl p-4 bg-purple_tertiary rounded-2xl shadow-md absolute top-0">
-        {"Hello"}
+    <div className="flex flex-col justify-start items-center min-h-screen bg-background relative space-y-4 p-4">
+      <div className="w-full max-w-2xl p-4 bg-purple_tertiary rounded-2xl shadow-md text-purple_primary flex">
+        <img src={MOH} alt="Notification 1" className="w-1/6 h-auto rounded-l-2xl" />
+        <div className="w-2/3 p-4">
+          {"Notification 1 content"}
+        </div>
+      </div>
+      <div className="w-full max-w-2xl p-4 bg-pink_tertiary rounded-2xl shadow-md text-pink_primary">
+        {"Notification 2"}
+      </div>
+      <div className="w-full max-w-2xl p-4 bg-blue_tertiary rounded-2xl shadow-md text-blue_primary">
+        {"Notification 3"}
+      </div>
+      <div className="w-full max-w-2xl p-4 bg-green_tertiary rounded-2xl shadow-md text-green_primary">
+        {"Notification 4"}
       </div>
     </div>
   );
