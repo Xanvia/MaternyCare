03f8b2f7c80096999c152f590729b3730c9fda24
Yasela-dissanayake <yasela2014@gmail.com>
2024-06-09 13:02:06 +0530

    feat: index.ts file updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,52 +1,31 @@
-import * as express from "express"
-import * as bodyParser from "body-parser"
-import { Request, Response } from "express"
-import { AppDataSource } from "./data-source"
-import { Routes } from "./routes"
-import { User } from "./entity/User"
+import app from "./app";
+import { AppDataSource } from "./data-source";
+import { User } from "./entity/User";
 
-AppDataSource.initialize().then(async () => {
+async function main() {
+  // start express server
+  app.listen(3000, () => {
+    console.log("Express server has started on port 3000.");
+  });
 
-    // create express app
-    const app = express()
-    app.use(bodyParser.json())
-
-    // register express routes from defined application routes
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next)
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
-
-            } else if (result !== null && result !== undefined) {
-                res.json(result)
-            }
-        })
+  // insert new users for test
+  await AppDataSource.manager.save(
+    AppDataSource.manager.create(User, {
+      firstName: "Timber",
+      lastName: "Saw",
+      age: 27,
     })
+  );
 
-    // setup express app here
-    // ...
+  await AppDataSource.manager.save(
+    AppDataSource.manager.create(User, {
+      firstName: "Phantom",
+      lastName: "Assassin",
+      age: 24,
+    })
+  );
 
-    // start express server
-    app.listen(3000)
+  console.log("Open http://localhost:3000/users to see results");
+}
 
-    // insert new users for test
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Timber",
-            lastName: "Saw",
-            age: 27
-        })
-    )
-
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Phantom",
-            lastName: "Assassin",
-            age: 24
-        })
-    )
-
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
-
-}).catch(error => console.log(error))
+main();
