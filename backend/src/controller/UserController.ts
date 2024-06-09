3c9697b53f0bb61ed1dd34cced48f2b236674a9a
Yasela-dissanayake <yasela2014@gmail.com>
2024-06-09 21:04:23 +0530

    feat: jwt middlewear file created

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -1,7 +1,11 @@
 import { AppDataSource } from "../data-source";
 import { NextFunction, Request, Response } from "express";
 import { User } from "../entity/User";
+import bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 export class UserController {
   private userRepository = AppDataSource.getRepository(User);
 
@@ -25,14 +29,42 @@ export class UserController {
   async save(request: Request, response: Response, next: NextFunction) {
     const { firstName, lastName, email, password } = request.body;
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = Object.assign(new User(), {
       firstName,
       lastName,
       email,
-      password,
+      password: hashedPassword,
     });
 
-    return this.userRepository.save(user);
+    const savedUser = await this.userRepository.save(user);
+
+    const token = jwt.sign({ userId: savedUser.id }, process.env.JWT_SECRET);
+
+    return { user: savedUser, token };
+  }
+
+  async login(request: Request, response: Response, next: NextFunction) {
+    const { email, password } = request.body;
+
+    const user = await this.userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      return response.status(400).json({ message: "Email does not exist" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return response.status(400).json({ message: "Password is incorrect" });
+    }
+
+    // Generate a JWT
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+
+    // Send the token back to the client
+    return { user, token };
   }
 
   async remove(request: Request, response: Response, next: NextFunction) {
---
+++ backend/src/middlewear/jwtMiddleware.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import * as jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
+
+export function jwtMiddleware(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const token = request.headers.authorization;
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Attach the decoded data to the request object
+    request.user = decoded;
+
+    next();
+  } catch (err) {
+    response.status(401).json({ message: "Invalid token" });
+  }
+}
