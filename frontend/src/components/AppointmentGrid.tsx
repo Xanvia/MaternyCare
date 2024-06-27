794ce881d59648c41509b5d97838a36311ed4866
Sahan-Maleesha <smaleesha84@gmail.com>
2024-06-28 02:37:29 +0530

    feat:Create Appointment cards

--- frontend/src/components/AppointmentGrid.tsx
+++ frontend/src/components/AppointmentGrid.tsx
@@ -11,13 +11,24 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-export default function Appointmets() {
+export default function Appointments() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
         {Array.from(Array(6)).map((_, index) => (
           <Grid item xs={2} sm={4} md={4} key={index}>
-            <Item>xs=2</Item>
+            <Item>
+              <div >2024</div>
+              <div>November</div>
+              <div className='border-2'></div>
+              <div className='flex flex-row items-center'>
+                <div>22</div>
+                <div className='border-2'></div>
+                <div>29</div>
+              </div>
+              <div>1st Home visit</div>
+            </Item>
+            
           </Grid>
         ))}
       </Grid>
--- frontend/src/views/Appointments.tsx
+++ frontend/src/views/Appointments.tsx
@@ -1,6 +1,12 @@
-const Appointments = () => {
-  return <div>Appointments</div>;
-  
+import * as React from 'react';
+import Appointments from '../components/AppointmentGrid'; // Adjust the path as necessary
+
+const App: React.FC = () => {
+  return (
+    <div className="mx-11 my ">
+      <Appointments />
+    </div>
+  );
 };
 
-export default Appointments;
+export default App;
