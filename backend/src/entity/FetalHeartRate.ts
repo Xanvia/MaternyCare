28266f58808730949a4a185e4620b1f42f13169a
Methmina Binara <binaraepa@gmail.com>
2024-07-01 12:45:09 +0530

    feat: Created Fetal Heart Rate entity

---
+++ backend/src/entity/FetalHeartRate.ts
@@ -0,0 +1,16 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+  } from "typeorm";
+  
+  @Entity()
+  export class Location {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column({ type: 'int' })
+    fetalHeartRate: number;
+
+  }
+  
