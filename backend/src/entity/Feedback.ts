333e8f19fcfb175a482e41d7bf043dbf9b8fa9a3
Sahan-Maleesha <smaleesha84@gmail.com>
2024-07-05 00:47:56 +0530

    feat:complete feedback entity

--- backend/src/entity/Feedback.ts
+++ backend/src/entity/Feedback.ts
@@ -5,20 +5,21 @@ import {
     ManyToOne,
   } from "typeorm";
 import { Mother } from "./Mother";
+import { Phm } from "./Phm";
   
   @Entity()
   export class FieldArea {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @ManyToOne(() => Mother, (mother) => mother.id)
-    m_id: Mother;
-  
-    @Column({ type: 'varchar', length: 255 })
-    district: string;
+    @ManyToOne(() => Mother, (mother) => mother.feedbacks)
+    mother: Mother;
+
+    @ManyToOne(() => Phm, (phm) => phm.feedbacks)
+    phm: Phm;
 
     @Column({ type: 'varchar', length: 255 })
-    field_area: string;
+    content: string;
 
   }
   
