35ad7a96814de102100fe74bea5a7d03364bd2c8
Sahan-Maleesha <smaleesha84@gmail.com>
2024-07-04 23:53:20 +0530

    feat:completed phm entity

--- backend/src/entity/Phm.ts
+++ backend/src/entity/Phm.ts
@@ -14,7 +14,32 @@ export class Phm {
   @PrimaryGeneratedColumn()
   id: number;
 
-  // other fields...
+  @Column({ type: 'int' })
+  phone_number: number;
+
+  @Column({ type: 'int' })
+  phm_id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  nic: string;
+
+  // @Column({ type: 'int' })
+  // fieldArea_id: number;
+
+  @Column({ type: 'boolean' })
+  isVisited: boolean;
+
+  @Column({ type: 'int' })
+  mother_count: number;
+
+  @Column({ type: 'int' })
+  baby_count: number;
+
+  @Column({ type: 'int' })
+  star_points: number;
+
+  @Column({ type: 'boolean' })
+  isVerified: boolean;
 
   @OneToMany(() => Appointment, (appointment) => appointment.phm)
   appointments: Appointment[];
