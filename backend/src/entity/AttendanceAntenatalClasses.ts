4b55e6c7dce6ae0d6f9a01ae86b1c0f323b00cd1
Methmina Binara <binaraepa@gmail.com>
2024-07-15 22:26:40 +0530

    feat: Created Attendance antenatal classes entity

---
+++ backend/src/entity/AttendanceAntenatalClasses.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Session {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'int' })
+  session_id: number;
+
+  @Column({ type: 'date' })
+  date: Date;
+
+  @Column({ type: 'varchar', length: 255 })
+  husband: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  wife: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  other: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  signature: string;
+}
