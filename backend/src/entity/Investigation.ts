b365994923abc5f669e5ae67c83c87c66961c8a1
Sahan-Maleesha <smaleesha84@gmail.com>
2024-07-15 18:48:06 +0530

    feat:created investigation entity

---
+++ backend/src/entity/Investigation.ts
@@ -0,0 +1,22 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+  } from "typeorm";
+  
+  @Entity()
+  export class FieldArea {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column({ type: 'varchar', length: 255 })
+    boold_sugar: string;
+
+    @Column({ type: 'varchar', length: 255 })
+    heamoglobin: string;
+
+    @Column({ type: 'varchar', length: 255 })
+    other_investigations: string;
+
+  }
+  
