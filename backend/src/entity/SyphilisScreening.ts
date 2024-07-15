ef2bea80e77bd9d0d6563c3c1002b40a8685e8e6
Sahan-Maleesha <smaleesha84@gmail.com>
2024-07-15 19:21:05 +0530

    feat:create syphilis Screening entity

---
+++ backend/src/entity/SyphilisScreening.ts
@@ -0,0 +1,28 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+  } from "typeorm";
+  
+  @Entity()
+  export class Location {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column({ type: 'int' })
+    POA_at_blood_sampling: number;
+
+    @Column({ type: 'date' })
+    Date_of_blood_sampling: Date;
+
+    @Column({ type: 'date' })
+    Date_of_result_received: Date;
+
+    @Column({ type: 'enum', enum: ['NR', 'R'] })
+    Result: 'NR' | 'R';
+
+    @Column({ type: 'date' })
+    if_r_date_of_referral: Date;
+
+  }
+  
