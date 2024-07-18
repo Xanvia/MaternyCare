f103817bd46f7cc9048a914cfa73f9c25468b717
Methmina Binara <binaraepa@gmail.com>
2024-07-18 21:35:05 +0530

    feat: Added new entities to data source file

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -7,6 +7,21 @@ import { Phm } from "./entity/Phm";
 import { Appointment } from "./entity/Appointment";
 import { FieldArea } from "./entity/FieldArea";
 import { Feedback } from "./entity/Feedback";
+import { AttendanceAntenatalClasses } from "./entity/AttendanceAntenatalClasses";
+import { DentalCare } from "./entity/DentalCare";
+import { FamilyHistory } from "./entity/FamilyHistory";
+import { FetalHeartRate } from "./entity/FetalHeartRate";
+import { HealthAssessment } from "./entity/HealthAssessment";
+import { Investigation } from "./entity/Investigation";
+import { KickCount } from "./entity/KickCount";
+import { Location } from "./entity/Location";
+import { MedicalSurgicalHistory } from "./entity/MedicalSurgicalHistory";
+import { Moh } from "./entity/Moh";
+import { PastObstetricHistory } from "./entity/PastObstetricHistory";
+import { PersonalInformation } from "./entity/PersonalInformation";
+import { SyphilisScreening } from "./entity/SyphilisScreening";
+import { TetanusToxoidImmunization } from "./entity/TetanusToxoidImmunization";
+import { WeightGainChart } from "./entity/WeightGainChart";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -17,7 +32,7 @@ export const AppDataSource = new DataSource({
   database: "test",
   synchronize: true,
   logging: false,
-  entities: [User, Notice, Mother, Phm, Appointment, Feedback, FieldArea],
+  entities: [User, Notice, Mother, Phm, Appointment, Feedback, FieldArea, AttendanceAntenatalClasses, DentalCare, FamilyHistory, FetalHeartRate, HealthAssessment, Investigation, KickCount, Location, MedicalSurgicalHistory, Moh, PastObstetricHistory, PersonalInformation, SyphilisScreening, TetanusToxoidImmunization, WeightGainChart],
   migrations: [],
   subscribers: [],
 });
--- backend/src/entity/KickCount.ts
+++ backend/src/entity/KickCount.ts
@@ -5,7 +5,7 @@ import {
   } from "typeorm";
   
   @Entity()
-  export class Location {
+  export class KickCount {
     @PrimaryGeneratedColumn()
     id: number;
   
