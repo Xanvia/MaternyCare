c3ef2174d79dd5ff8c4202a59f1d20a83722a60d
Yasela-dissanayake <yasela2014@gmail.com>
2024-07-24 14:19:59 +0530

    user and phm controllers updated

---
+++ backend/src/controller/PhmController.ts
@@ -0,0 +1,70 @@
+import { AppDataSource } from "../data-source";
+import { NextFunction, Request, Response } from "express";
+import { Phm } from "../entity/Phm";
+
+export class PhmController {
+  private phmRepository = AppDataSource.getRepository(Phm);
+
+  async all(request: Request, response: Response, next: NextFunction) {
+    return this.phmRepository.find();
+  }
+
+  async one(request: Request, response: Response, next: NextFunction) {
+    const id = parseInt(request.params.id);
+
+    const phm = await this.phmRepository.findOne({
+      where: { id },
+    });
+
+    if (!phm) {
+      return "unlisted phm";
+    }
+    return phm;
+  }
+
+  async save(request: Request, response: Response, next: NextFunction) {
+    const {
+      phone_number,
+      phm_id,
+      nic,
+      mother_count,
+      baby_count,
+      star_points,
+      isVerified,
+      appointments,
+      feedbacks,
+      user,
+      fielaArea,
+    } = request.body;
+
+    const phm = Object.assign(new Phm(), {
+      phone_number,
+      phm_id,
+      nic,
+      mother_count,
+      baby_count,
+      star_points,
+      isVerified,
+      appointments,
+      feedbacks,
+      user,
+      fielaArea,
+    });
+
+    return this.phmRepository.save(phm);
+  }
+
+  async remove(request: Request, response: Response, next: NextFunction) {
+    const id = parseInt(request.params.id);
+
+    let phmToRemove = await this.phmRepository.findOneBy({ id });
+
+    if (!phmToRemove) {
+      return "this phm not exist";
+    }
+
+    await this.phmRepository.remove(phmToRemove);
+
+    return "phm has been removed";
+  }
+}
