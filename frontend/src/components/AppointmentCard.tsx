580f39f27362287732459f944b3fddf3a5504e9f
Sahan-Maleesha <smaleesha84@gmail.com>
2024-07-24 21:19:53 +0530

    feat:Make the appointment page mobile responsive.

--- frontend/src/components/AppointmentCard.tsx
+++ frontend/src/components/AppointmentCard.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
 
-const AppointmentCard: React.FC = () => {
+interface AppointmentCardProps {
+  year: number;
+  month: string;
+  start_day: number;
+  end_day: number;
+  title: string;  // appointment title or description
+ 
+}
+const AppointmentCard: React.FC<AppointmentCardProps> = ({year, month, start_day, end_day, title}) => {
   return (
-    <div className='w-36 bg-white rounded-3xl'>
-      <div className='flex flex-col items-center justify-center pt-1'>
-        <header className='text-base text-blue_primary mt-1'>2024</header>
-        <header className='text-xl md:text-xl sm:text-lg xs:text-base text-pink_primary font-bold'>November</header>
-      </div>
-      <div className='flex justify-center'>
-        <hr className='mt-2 w-11/12 border-1 border-gray-300' />
-      </div>
-      <div className='flex flex-row items-center justify-center mt-2 m-2'>
-        <div className='md:text-3xl sm:text-3xl xs:text-2xl text-blue_primary font-bold'>22</div>
-        <hr className='w-2 border-2 border-gray-300 mx-1'/>
-        <div className='md:text-3xl sm:text-3xl xs:text-2xl text-blue_primary font-bold'>29</div>
-      </div>
-      <header className=' flex mt-2 justify-center pb-1 text-pink_primary text-sm'>1st Home visit</header>
+    <div className='sm:w-36 w-full bg-white rounded-3xl pb-2'>
+      <button  className='sm:w-36 w-full  items-center justify-center'>
+        <div  className='flex flex-col items-center justify-center pt-1'>
+          <header className='xs:text-base text-blue_primary mt-1 text-lg'>{year}</header>
+          <header className='text-xl md:text-xl sm:text-lg xs:text-base text-pink_primary font-bold'>{month}</header>
+        </div>
+        <div className='flex justify-center w-full'>
+          <hr className='mt-2 w-10/12 border-1 border-gray-300' />
+        </div>
+        <div className='flex flex-row items-center justify-center mt-2 m-2'>
+          <div className='md:text-3xl sm:text-3xl xs:text-2xl text-3xl text-blue_primary font-bold'>{start_day}</div>
+          <hr className='w-2 border-2 border-gray-300 mx-1'/>
+          <div className='md:text-3xl sm:text-3xl xs:text-2xl text-3xl text-blue_primary font-bold'>{end_day}</div>
+        </div>
+        <header className=' flex mt-2 justify-center pb-1 text-pink_primary text-sm'>{title}</header>
+      </button>
+      
     </div>
   );
 };
--- frontend/src/views/Appointments.tsx
+++ frontend/src/views/Appointments.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 // import Appointments from '../components/AppointmentGrid'; // Adjust the path as necessary
 import AppointmentCard from '../components/AppointmentCard';
+import { appointments } from '../data/Data';
 
 const App: React.FC = () => {
   return (
-    <div className="mx-11 my ">
-      {/* <Appointments /> */}
-      <AppointmentCard />
+    <div className='flex justify-center'>
+      <div className="w-full mx-11 my grid xl:grid-cols-5 lg:grid-cols-4 sm:grid-cols-3 md:grid-cols-4 xs:grid-cols-2 grid-cols-1 xs:gap-y-10 xs:gap-x-16 gap-y-5 xs:px-10 justify-center">
+      {appointments.map((item)=>(
+        <AppointmentCard year={item.year} month={item.month}  start_day={item.start_day} end_day={item.end_day} title={item.title}/>
+        
+      ))}
+      </div>
     </div>
+    
   );
 };
 
