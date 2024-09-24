ed0aa2a2fa7d3575f757679760e82d799a9cd9bc
YaselaD <85230521+Yasela-dissanayake@users.noreply.github.com>
2024-09-24 14:14:15 +0530

    Rightbar profile integration 1 (#26)
    * feat: profile icon relaced with letters
    
    * rightbar profile section imtegrated

--- frontend/src/components/RightBarProfile.tsx
+++ frontend/src/components/RightBarProfile.tsx
@@ -6,9 +6,15 @@ import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import Logout from "@mui/icons-material/Logout";
-import Settings from "@mui/icons-material/Settings";
+// import Settings from "@mui/icons-material/Settings";
+import Person3Icon from "@mui/icons-material/Person3";
+import ToTitle from "./CaseConverter";
+import { useNavigate } from "react-router-dom";
 
 export default function RightBarProfile() {
+  let userItem = localStorage.getItem("user");
+  const user = userItem ? JSON.parse(userItem) : null;
+
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
@@ -16,6 +22,19 @@ export default function RightBarProfile() {
   };
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  const navigate = useNavigate();
+
+  const handleProfileClick = () => {
+    handleClose();
+    navigate("/profile");
+  };
+
+  const logout = () => {
+    handleClose();
+    localStorage.removeItem("token");
+    window.location.href = "/login";
   };
 
   return (
@@ -31,10 +50,24 @@ export default function RightBarProfile() {
             aria-expanded={open ? "true" : undefined}
           >
             <div className="bg-[#EAEEFD] flex justify-center items-center rounded-xl w-16 h-16 p-2">
-              <img
+              {/* <img
                 className="rounded-full border-2 border-white "
                 src="https://randomuser.me/api/portraits/women/94.jpg"
-              />
+              /> */}
+              {user.image ? (
+                <img
+                  className="rounded-full border-2 border-white "
+                  src="https://randomuser.me/api/portraits/women/94.jpg"
+                />
+              ) : (
+                <div className="relative inline-flex items-center justify-center w-12 h-12 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                  <span className="font-medium text-xl text-gray-600 dark:text-gray-300">
+                    {`${ToTitle(user.firstName[0])} ${ToTitle(
+                      user.lastName[0]
+                    )}`}
+                  </span>
+                </div>
+              )}
             </div>
           </IconButton>
         </Tooltip>
@@ -74,13 +107,13 @@ export default function RightBarProfile() {
         transformOrigin={{ horizontal: "right", vertical: "top" }}
         anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
-        <MenuItem onClick={handleClose}>
+        <MenuItem onClick={handleProfileClick}>
           <ListItemIcon>
-            <Settings fontSize="small" />
+            <Person3Icon fontSize="small" />
           </ListItemIcon>
           Profile
         </MenuItem>
-        <MenuItem onClick={handleClose}>
+        <MenuItem onClick={logout}>
           <ListItemIcon>
             <Logout fontSize="small" />
           </ListItemIcon>
