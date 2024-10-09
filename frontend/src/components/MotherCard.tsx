126db0eb4a8abc6cfd4d90c3886ae8abf4599311
Yasela-dissanayake <yasela2014@gmail.com>
2024-10-09 10:06:42 +0530

    style: added more styles to mother card in phm dashboard

--- frontend/src/components/MotherCard.tsx
+++ frontend/src/components/MotherCard.tsx
@@ -23,7 +23,7 @@ const MotherCard: React.FC<MotherCardProps> = ({
   const [isAdded, setIsAdded] = React.useState(false);
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white border border-gray-200 p-4">
+    <div className="max-w-sm rounded-md overflow-hidden shadow-lg bg-white border border-gray-200 p-4">
       <div className="grid grid-cols-4 gap-6">
         <div className="flex justify-center items-center px-2 py-0 col-span-1">
           <div className="relative inline-flex items-center justify-center w-14 h-14 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
@@ -34,12 +34,10 @@ const MotherCard: React.FC<MotherCardProps> = ({
         </div>
 
         <div className="text-start col-span-2">
-          <h2 className="text-xl font-semibold mb-2">{firstName}</h2>
-          <p className="text-gray-600 mb-1">NIC: {nic}</p>
+          <h2 className="text-xl font-semibold mb-1">{firstName}</h2>
+          <p className="text-gray-600 ">NIC: {nic}</p>
           <p className="text-gray-600 mb-4">
-            <span>
-              <LocationIcon />{" "}
-            </span>
+            <span>{/* <LocationIcon />{" "} */}</span>
             {location}
           </p>
         </div>
