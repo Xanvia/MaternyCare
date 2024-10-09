f4098ff2b2d3498d8d29090c53b5b9e21456da0b
Methmina Binara <binaraepa@gmail.com>
2024-10-09 14:20:02 +0530

    WIP:Table in progress

--- frontend/src/views/DashboardMOH.tsx
+++ frontend/src/views/DashboardMOH.tsx
@@ -4,6 +4,7 @@ import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 import AddMotherModal from '../modals/AddMotherModal';
 import CustomPaginationActionsTable from '../components/CustomPaginationActionsTable';
+import PatientTable from '../components/PatientTable';
 
 const DashboardMOH = () => {
   const [value, setValue] = React.useState('mother'); // Default tab is 'MOTHER LIST'
@@ -16,8 +17,9 @@ const DashboardMOH = () => {
     <div>
       <h1 className='font-sans text-lg text-text_color_2 ml-5'>Dashboard Overview</h1>
 
+      <PatientTable />
       {/* Tabs to switch between Mother, PHM, and Pending List */}
-      <div className='flex flex-row justify-center border-b mb-5 mt-8'>
+      {/* <div className='flex flex-row justify-center border-b mb-5 mt-8'>
         <Box sx={{ width: '100%' }}>
           <Tabs
             value={value}
@@ -32,10 +34,10 @@ const DashboardMOH = () => {
             <Tab value="pending" label="PENDING LIST" />
           </Tabs>
         </Box>
-      </div>
+      </div> */}
 
       {/* Search bar and add mother button */}
-      <div className="flex bg-white w-full justify-between items-center p-4">
+      {/* <div className="flex bg-white w-full justify-between items-center p-4">
         <div className="flex space-x-4">
           <button className="px-5">Patient List</button>
           <button className="px-5">Red Patient List</button>
@@ -50,14 +52,14 @@ const DashboardMOH = () => {
           />
           <AddMotherModal />
         </div>
-      </div>
+      </div> */}
 
       {/* Tab Panels with the Custom Table */}
-      <div className="mt-5 px-4">
-        {value === 'mother' && <CustomPaginationActionsTable />} {/* Mother List */}
-        {value === 'phm' && <CustomPaginationActionsTable />}    {/* PHM List */}
-        {value === 'pending' && <CustomPaginationActionsTable />} {/* Pending List */}
-      </div>
+      {/* <div className="mt-5 px-4">
+        {value === 'mother' && <CustomPaginationActionsTable />}
+        {value === 'phm' && <CustomPaginationActionsTable />}
+        {value === 'pending' && <CustomPaginationActionsTable />}
+      </div> */}
     </div>
   );
 };
