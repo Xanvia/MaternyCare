9d8443618d2302238a2b0c3622ba606b8dc475f0
Methmina Binara <binaraepa@gmail.com>
2024-10-09 18:18:26 +0530

    WIP

--- frontend/src/components/PatientTable.tsx
+++ frontend/src/components/PatientTable.tsx
@@ -112,7 +112,7 @@ export default function PatientTable() {
         </div>
         <div className="overflow-x-auto border rounded">
           <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-50">
+            <thead className="bg-purple_secondary">
               <tr>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Patient</th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Address</th>
