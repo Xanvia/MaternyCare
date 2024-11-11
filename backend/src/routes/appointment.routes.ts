afeffe5557c4eafb2506581f3f278a7003911c9f
Sahan Maleesha <smaleesha84@gmail.com>
2024-11-12 00:52:54 +0530

    fix:fixed issue with appointment controller

--- backend/src/routes/appointment.routes.ts
+++ backend/src/routes/appointment.routes.ts
@@ -42,7 +42,7 @@ export const AppointmentRoutes = [
     route: "/appointments/:id",
     controller: AppointmentController,
     action: "update",
-    middlewares: [jwtMiddleware],
+    middlewares: [],
   },
   {
     method: "delete",
