2651bff7ee9d61a4b1faff79330dad78b0b45b59
Sahan Maleesha <smaleesha84@gmail.com>
2024-11-13 16:40:35 +0530

    feat:create appointment page to view single-mother appointments

---
+++ frontend/src/views/PhmSingleMotherAppointment.tsx
@@ -0,0 +1,14 @@
+import Appointments from "./Appointments";
+
+const PhmSingleMotherAppointment = () => {
+  
+  return (
+    <div>
+      
+      <Appointments/>
+      
+    </div>
+  );
+};
+
+export default PhmSingleMotherAppointment;
