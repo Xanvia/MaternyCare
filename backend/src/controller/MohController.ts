56a1dbc7d4c7fb92613a11dbf800b6bcc2b08b66
Methmina Binara <binaraepa@gmail.com>
2024-11-17 17:20:38 +0530

    fix:MOH controller

--- backend/src/controller/MohController.ts
+++ backend/src/controller/MohController.ts
@@ -89,41 +89,41 @@ export class MohController {
     return "MOH has been removed";
   }
   //----------------------------------------------------------------------
-  async addMother(request: Request, response: Response, next: NextFunction) {
-    const userId = request.user?.userId;
+  // async addMother(request: Request, response: Response, next: NextFunction) {
+  //   const userId = request.user?.userId;
 
-    const parsedUserId = parseInt(userId, 10);
+  //   const parsedUserId = parseInt(userId, 10);
 
-    const user = await this.userRepository.findOne({
-      where: { id: parsedUserId },
-    });
+  //   const user = await this.userRepository.findOne({
+  //     where: { id: parsedUserId },
+  //   });
 
-    const phm = await this.phmRepository.findOne({
-      where: { user },
-      relations: ["user"],
-    });
+  //   const phm = await this.phmRepository.findOne({
+  //     where: { user },
+  //     relations: ["user"],
+  //   });
 
-    const motherId = request.body.motherID; // ID of the mother to be added
+  //   const motherId = request.body.motherID; // ID of the mother to be added
 
-    const mother = await this.motherRepository.findOne({
-      where: { id: motherId },
-    });
+  //   const mother = await this.motherRepository.findOne({
+  //     where: { id: motherId },
+  //   });
 
-    console.log("ad mother: " + mother.age);
+  //   console.log("ad mother: " + mother.age);
 
-    if (!phm) {
-      return response.status(404).json({ error: "PHM not found" });
-    }
+  //   if (!phm) {
+  //     return response.status(404).json({ error: "PHM not found" });
+  //   }
 
-    if (!mother) {
-      return response.status(404).json({ error: "Mother not found" });
-    }
+  //   if (!mother) {
+  //     return response.status(404).json({ error: "Mother not found" });
+  //   }
 
-    mother.phm = phm; // Assign the mother to the PHM
-    return this.motherRepository.save(mother);
+  //   mother.phm = phm; // Assign the mother to the PHM
+  //   return this.motherRepository.save(mother);
 
-    // return response
-    //   .status(200)
-    //   .json({ message: "Mother added to PHM successfully" });
-  }
+  //   // return response
+  //   //   .status(200)
+  //   //   .json({ message: "Mother added to PHM successfully" });
+  // }
 }
