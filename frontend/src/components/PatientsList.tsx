4df0ac6552517a97a88b57951710136d09500754
Sahan-Maleesha <160100516+Sahan-Maleesha@users.noreply.github.com>
2024-12-01 20:20:14 +0530

    Create shared mother interface (#61)

--- frontend/src/components/PatientsList.tsx
+++ frontend/src/components/PatientsList.tsx
@@ -1,28 +1,29 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Mother } from "./Mother";
 
-interface Mother {
-  id: number;
-  nic: string;
-  phone_number: number;
-  location: string;
-  delivery_date: string;
-  user: {
-    firstName: string;
-    lastName: string;
-  };
-  phm: {};
-  appointments: {
-    id: number;
-    appointment_type: string;
-    startDate: string;
-    endDate: string;
-    fixedDate: string;
-    month: string;
-    checkedByMother: boolean;
-    checkedByPHM: boolean;
-  }[];
-}
+// interface Mother {
+//   id: number;
+//   nic: string;
+//   phone_number: number;
+//   location: string;
+//   delivery_date: string;
+//   user: {
+//     firstName: string;
+//     lastName: string;
+//   };
+//   phm: {};
+//   appointments: {
+//     id: number;
+//     appointment_type: string;
+//     startDate: string;
+//     endDate: string;
+//     fixedDate: string;
+//     month: string;
+//     checkedByMother: boolean;
+//     checkedByPHM: boolean;
+//   }[];
+// }
 
 interface PatientsListProps {
   mothers: Mother[];
