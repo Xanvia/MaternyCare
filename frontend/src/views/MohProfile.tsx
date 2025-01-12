9a0b9cb7bea852809d84c005da5fea47f6e1b291
Methmina Binara <binaraepa@gmail.com>
2025-01-12 13:59:07 +0530

    feat: display MOH profile picture as initials of the name

--- frontend/src/views/MohProfile.tsx
+++ frontend/src/views/MohProfile.tsx
@@ -1,8 +1,5 @@
 import { useEffect, useState } from "react";
 import { EyeIcon, EyeOffIcon } from "../assets/icons/Icons";
-// import EditPersonalInfo from "../modals/PersonalInfoEditPopup";
-// import EditAccountInfo from "../modals/AccountInfoEditPopup";
-// import EditLocationInfo from "../modals/LocationInfoEditPopup";
 import ToTitle from "../components/CaseConverter";
 import axios from "axios";
 import { CircularProgress } from "@mui/material";
@@ -15,7 +12,8 @@ interface Moh {
     lastName: string;
     email: string;
     password: string;
-  } 
+    profileImage?: string; // Add profileImage property
+  };
   NIC: string;
   phoneNumber: string;
   mohArea: string;
@@ -28,11 +26,8 @@ const MohProfile = () => {
   const user = userItem ? JSON.parse(userItem) : null;
 
   const BASE_URL = "http://localhost:3000/";
-  // const [moh, setMoh] = useState([]);
   const [moh, setMoh] = useState<Moh>();
   const [loading, setLoading] = useState(false);
-
-  // const token = JSON.parse(localStorage.getItem("token"));
 
   const storedToken = localStorage.getItem("token");
   const token = storedToken ? JSON.parse(storedToken) : null;
@@ -49,8 +44,7 @@ const MohProfile = () => {
       };
       axios(axiosConfig)
         .then((response) => {
-          console.log(response.data); // Debug the structure
-          setMoh(response.data); // Save the entire response to state
+          setMoh(response.data);
         })
         .catch((err) => {
           console.error("Error fetching MOH data:", err);
@@ -59,37 +53,38 @@ const MohProfile = () => {
           setLoading(false);
         });
     };
-  
+
     getMoh();
-  }, []);
-  
+  }, [user.id, token]);
 
   if (loading) {
     return <CircularProgress />;
   }
 
-  console.log("check", moh);
-  console.log("check1", user);
+  const initials = `${moh?.user.firstName.charAt(0)}${moh?.user.lastName.charAt(0)}`;
 
   return (
     <div className="xs:mx-10 mx-3 bg-white rounded-xl p-5 flex flex-col gap-8">
       <div className="border-solid border-2 rounded-lg md:py-2 px-5 sm:flex justify-between items-center py-5">
         <div className="flex flex-col items-center bg-white xs:flex-row xs:max-w-xl">
-          <img
-            className="object-cover w-24 rounded-full h-24"
-            src="https://randomuser.me/api/portraits/lego/3.jpg"
-            alt=""
-          />
-          <div className="flex flex-col  justify-between p-4 leading-normal">
+          {moh?.user.profileImage ? (
+            <img
+              className="object-cover w-24 rounded-full h-24"
+              src={moh.user.profileImage}
+              alt="Profile"
+            />
+          ) : (
+            <div className="flex items-center justify-center w-24 h-24 bg-gray-300 rounded-full">
+              <span className="text-3xl font-bold text-white">{initials}</span>
+            </div>
+          )}
+          <div className="flex flex-col justify-between p-4 leading-normal">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-text_color_1 dark:text-white">
               {`${moh?.user.firstName} ${moh?.user.lastName}`}
             </h5>
             <p className="mb-1 font-normal text-text_color_2 dark:text-gray-400">
               Medical Officer of Health (MOH)
             </p>
-            {/* <p className="mb-1 font-normal text-text_color_2 dark:text-gray-400">
-              Badulla, Srilanka
-            </p> */}
           </div>
         </div>
       </div>
@@ -127,7 +122,6 @@ const MohProfile = () => {
             <h5 className="">MOH Area</h5>
             <p className="font-semibold mt-2 mb-4">{moh?.mohArea || "N/A"}</p>
           </div>
-
         </div>
       </div>
       <div className="border-solid border-2 rounded-lg py-5 px-5 ">
@@ -147,7 +141,7 @@ const MohProfile = () => {
                 type={showPassword ? "text" : "password"}
                 id="password"
                 className="bg-gray-50 border border-none pl-0 text-gray-900 text-sm rounded-lg disabled:opacity-50 focus:ring-0 focus:outline-none w-full"
-                value={(user.password)}
+                value={user.password}
                 required
               />
               <button
@@ -163,7 +157,6 @@ const MohProfile = () => {
               </button>
             </div>
           </div>
-          
         </div>
       </div>
     </div>
@@ -171,6 +164,3 @@ const MohProfile = () => {
 };
 
 export default MohProfile;
-// function setLoading(arg0: boolean) {
-//   throw new Error("Function not implemented.");
-// }
