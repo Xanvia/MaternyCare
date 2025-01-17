2f47bc6ffe7fc29c7517860dbb4c3b3a273143be
Yasela-dissanayake <yasela2014@gmail.com>
2025-01-17 11:20:48 +0530

    red list dashboard improved

--- frontend/src/views/SingleRedMother.tsx
+++ frontend/src/views/SingleRedMother.tsx
@@ -150,8 +150,8 @@ const SingleRedMother = () => {
   };
 
   return (
-    <div>
-      <div className="grid grid-cols-2 w-1/4 py-4 bg-red-100 mx-4">
+    <div className="">
+      <div className="grid grid-cols-2 w-1/4 py-4 rounded-lg bg-pink_tertiary mx-4 ">
         <div className="flex justify-center items-center px-2 py-0 col-span-1">
           <div className="relative inline-flex items-center justify-center w-14 h-14 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
             <span className="font-medium text-xl text-gray-600 dark:text-gray-300">
