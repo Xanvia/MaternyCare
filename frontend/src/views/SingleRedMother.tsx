0b9e59f45b26151b4f542688a05b3d740f1b6503
Yasela-dissanayake <yasela2014@gmail.com>
2025-01-17 15:38:16 +0530

    fix: vog dashboard color fixed

--- frontend/src/views/SingleRedMother.tsx
+++ frontend/src/views/SingleRedMother.tsx
@@ -182,7 +182,7 @@ const SingleRedMother = () => {
         />
         <button
           onClick={saveContent}
-          className="sm:mt-14 mt-20 px-4 py-2 bg-blue-500 text-white rounded-md"
+          className="sm:mt-14 mt-20 px-4 py-2 bg-green_primary text-white rounded-md"
         >
           Save
         </button>
@@ -222,7 +222,7 @@ const SingleRedMother = () => {
                 <button
                   type="button"
                   onClick={saveSignature}
-                  className="px-4 py-2 bg-green-500 text-white rounded-md"
+                  className="px-4 py-2 bg-green_primary text-white rounded-md"
                 >
                   Save
                 </button>
