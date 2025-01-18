407b0a626fb94f3c0501de986ce25f4e2a1ccce6
Yasela-dissanayake <yasela2014@gmail.com>
2025-01-18 17:51:16 +0530

    feat: device entity created

---
+++ backend/src/entity/DeviceData.ts
@@ -0,0 +1,24 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class DeviceData {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column("float")
+  heartRate: number;
+
+  @Column("float", { nullable: true })
+  signalQuality: number;
+
+  @Column()
+  isScanning: boolean;
+
+  @CreateDateColumn()
+  timestamp: Date;
+}
