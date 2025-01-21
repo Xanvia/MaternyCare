0745a0c26c8d8b83b9d0f5274fed315448699893
Yasela-dissanayake <yasela2014@gmail.com>
2025-01-21 20:17:43 +0530

    feat: basic details preview completed

--- frontend/src/views/FormPreview.tsx
+++ frontend/src/views/FormPreview.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+// import React from "react";
 import BasicDetailsPreview from "./forms/BasicDetailsPreview";
+import { useParams } from "react-router-dom";
 
 const FormPreview = () => {
+  const { id = '' } = useParams<{ id: string }>() || {};
+
   return (
     <div className="mx-4">
-      <BasicDetailsPreview />
+      <BasicDetailsPreview motherId={id} />
     </div>
   );
 };
