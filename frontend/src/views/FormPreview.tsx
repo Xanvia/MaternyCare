acbbab7fe0533acf839f9c137de34de54fd323ab
Yasela-dissanayake <yasela2014@gmail.com>
2025-01-22 07:05:41 +0530

    feat: BMI chart integrated

--- frontend/src/views/FormPreview.tsx
+++ frontend/src/views/FormPreview.tsx
@@ -1,13 +1,21 @@
 // import React from "react";
 import BasicDetailsPreview from "./forms/BasicDetailsPreview";
 import { useParams } from "react-router-dom";
+import PresentObstetricHistoryPreview from "./forms/PresentObstetricHistoryPreview";
+import PregnancyBMIChart from "../components/PregnancyBMIChart";
+import SFHChart from "../components/SFHChart";
 
 const FormPreview = () => {
-  const { id = '' } = useParams<{ id: string }>() || {};
+  const { id = "" } = useParams<{ id: string }>() || {};
 
   return (
     <div className="mx-4">
       <BasicDetailsPreview motherId={id} />
+      <PresentObstetricHistoryPreview motherId={id} />
+      <div className="my-10">
+        <PregnancyBMIChart motherId={id} />
+      </div>
+      <SFHChart />
     </div>
   );
 };
