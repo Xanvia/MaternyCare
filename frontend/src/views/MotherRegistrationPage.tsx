aa0f45be62d0b893d79f099b940d620f19454fca
Methmina Binara <binaraepa@gmail.com>
2025-01-25 16:53:54 +0530

    fix:Mother registration page styles change

--- frontend/src/views/MotherRegistrationPage.tsx
+++ frontend/src/views/MotherRegistrationPage.tsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import logo from "../assets/images/logo.png";
+import { ErrorIcon } from "../assets/icons/Icons";
 
 // Validation schema
 const registrationSchema = Yup.object({
@@ -66,162 +67,67 @@ const MotherRegistrationPage: React.FC = () => {
   });
 
   return (
-    <div className="flex justify-center items-center h-screen bg-white">
+    <div className="flex items-center justify-center md:min-h-screen overflow-auto py-4 sm:pt-0 mt-8">
       <ToastContainer />
       <form
+        autoComplete="off"
         onSubmit={formik.handleSubmit}
-        className="space-y-4 w-full max-w-md p-8 bg-white rounded-lg lg:max-w-lg shadow-md"
+        className="flex flex-col items-center w-full max-w-md"
       >
-        <div className="flex flex-col justify-center items-center mt-10">
-          <img src={logo} alt="Materny Logo" className="w-24" />
-        </div>
-
-        <div className="text-center leading-9 font-semibold mt-5">
-          <h1 className="text-3xl md:text-4xl" style={{ fontFamily: "Ubuntu" }}>
-            <span style={{ color: "#0D99FF" }}>Materny</span>
-            <span style={{ color: "#F580AB" }}>Care</span>
-          </h1>
-        </div>
-
-        {/* Age Input */}
-        <div className="w-full">
-          <label
-            htmlFor="age"
-            className="block text-sm font-medium text-gray-700 mb-1"
+        <img
+          src={logo}
+          alt="MaternyCare Logo"
+          className="w-16 h-auto mb-4 mt-4 sm:mt-0" // Adjusted size classes
+        />
+        <header className="text-blue_primary lg:text-2xl ss:text-2xl text-lg lg:mb-4 mb-2">
+          Materny<span className="text-pink_primary">Care</span>
+        </header>
+        {[
+          { field: "age", placeholder: "Age" },
+          { field: "phone_1", placeholder: "Phone Number" },
+          { field: "bio", placeholder: "Bio" },
+          { field: "address", placeholder: "Address" },
+          { field: "delivery_date", placeholder: "Expected Delivery Date" },
+        ].map(({ field, placeholder }) => (
+          <div
+            key={field}
+            className="w-full flex flex-col items-center lg:mb-3 mb-2"
           >
-            Age
-          </label>
-          <input
-            type="number"
-            name="age"
-            id="age"
-            className={`block w-full p-2 border rounded-lg shadow-md ${
-              formik.touched.age && formik.errors.age
-                ? "border-red-500"
-                : "border-gray-300"
+            <label
+              htmlFor={field}
+              className="w-11/12 text-gray-700 lg:text-sm md:text-xs sm:text-xs text-xs mb-1"
+            >
+              {placeholder}
+            </label>
+            <input
+              className={`shadow appearance-none rounded-b-xl py-2 px-2 w-11/12 text-gray-700 leading-tight focus:shadow-outline 
+              lg:text-sm md:text-xs sm:text-xs text-xs
+            ${
+              formik.touched[field as keyof typeof formik.values] &&
+              formik.errors[field as keyof typeof formik.values]
+                ? "border-solid border-red-500"
+                : "border-none"
             }`}
-            value={formik.values.age}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          {formik.touched.age && formik.errors.age && (
-            <div className="text-red-500 text-sm mt-1">{formik.errors.age}</div>
-          )}
-        </div>
-
-        {/* Mobile Phone Number Input */}
-        <div className="w-full">
-          <label
-            htmlFor="phone_1"
-            className="block text-sm font-medium text-gray-700 mb-1"
-          >
-            Mobile Phone Number
-          </label>
-          <input
-            type="text"
-            name="phone_1"
-            id="phone_1"
-            className={`block w-full p-2 border rounded-lg shadow-md ${
-              formik.touched.phone_1 && formik.errors.phone_1
-                ? "border-red-500"
-                : "border-gray-300"
-            }`}
-            value={formik.values.phone_1}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          {formik.touched.phone_1 && formik.errors.phone_1 && (
-            <div className="text-red-500 text-sm mt-1">
-              {formik.errors.phone_1}
+              value={formik.values[field as keyof typeof formik.values]}
+              placeholder={field !== "delivery_date" ? placeholder : ""}
+              id={field}
+              name={field}
+              type={field === "delivery_date" ? "date" : "text"}
+              onChange={formik.handleChange}
+            />
+            <div className="w-11/12 mb-1 flex flex-col items-start mt-1">
+              {formik.touched[field as keyof typeof formik.values] &&
+              formik.errors[field as keyof typeof formik.values] ? (
+                <div className="text-red-500 text-xs">
+                  <ErrorIcon /> {formik.errors[field as keyof typeof formik.values]}
+                </div>
+              ) : null}
             </div>
-          )}
-        </div>
-
-        {/* Bio Input */}
-        <div className="w-full">
-          <label
-            htmlFor="bio"
-            className="block text-sm font-medium text-gray-700 mb-1"
-          >
-            Bio
-          </label>
-          <input
-            type="text"
-            name="bio"
-            id="bio"
-            className={`block w-full p-2 border rounded-lg shadow-md ${
-              formik.touched.bio && formik.errors.bio
-                ? "border-red-500"
-                : "border-gray-300"
-            }`}
-            value={formik.values.bio}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          {formik.touched.bio && formik.errors.bio && (
-            <div className="text-red-500 text-sm mt-1">{formik.errors.bio}</div>
-          )}
-        </div>
-
-        {/* Address Input */}
-        <div className="w-full">
-          <label
-            htmlFor="address"
-            className="block text-sm font-medium text-gray-700 mb-1"
-          >
-            Address
-          </label>
-          <textarea
-            name="address"
-            id="address"
-            rows={3}
-            className={`block w-full p-2 border rounded-lg shadow-md ${
-              formik.touched.address && formik.errors.address
-                ? "border-red-500"
-                : "border-gray-300"
-            }`}
-            value={formik.values.address}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          {formik.touched.address && formik.errors.address && (
-            <div className="text-red-500 text-sm mt-1">
-              {formik.errors.address}
-            </div>
-          )}
-        </div>
-
-        {/* Delivery Date Input */}
-        <div className="w-full">
-          <label
-            htmlFor="delivery_date"
-            className="block text-sm font-medium text-gray-700 mb-1"
-          >
-            Delivery Date
-          </label>
-          <input
-            type="date"
-            name="delivery_date"
-            id="delivery_date"
-            className={`block w-full p-2 border rounded-lg shadow-md ${
-              formik.touched.delivery_date && formik.errors.delivery_date
-                ? "border-red-500"
-                : "border-gray-300"
-            }`}
-            value={formik.values.delivery_date}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          {formik.touched.delivery_date && formik.errors.delivery_date && (
-            <div className="text-red-500 text-sm mt-1">
-              {formik.errors.delivery_date}
-            </div>
-          )}
-        </div>
-
+          </div>
+        ))}
         <button
+          className="py-3 rounded-xl w-11/12 text-white h-12 bg-blue_primary hover:bg-[#33C2FF] lg:text-sm md:text-sm sm:text-xs text-xs"
           type="submit"
-          className="w-full p-3 text-white bg-blue-600 rounded-lg hover:bg-blue-700 shadow-md"
         >
           Register
         </button>
